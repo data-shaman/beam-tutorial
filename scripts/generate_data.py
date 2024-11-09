0c26ae28a2c6965a037a9f2bbbd7655fcffa56ad
data-shaman <lsirghii@datashaman.co.uk>
2024-11-09 15:41:14 +0000

    Implemented correct datatype matching between avro and postreSQL.

--- scripts/generate_data.py
+++ scripts/generate_data.py
@@ -1,33 +1,62 @@
 import os
+import fastavro
 import psycopg2
-from datetime import datetime, timedelta
-import json
+from psycopg2 import sql
 from fake_web_events import Simulation
 
-
-# Define the toal number of days and events per day
-TOTAL_DAYS = 31
-EVENTS_PER_DAY = 1000
-TOTAL_EVENTS = TOTAL_DAYS * EVENTS_PER_DAY
-
-# Set the starting date (adjust is you want a different start date)
-start_date = datetime.now() - timedelta(days=TOTAL_DAYS)
-
-# Initialise the simulation
-simulation = Simulation(
-    user_pool_size=int(TOTAL_EVENTS // 10),
-    sessions_per_day=int(TOTAL_EVENTS)
-    )
-
-# Set the simulation's current time to start from `start_date`
-simulation.cur_time = start_date
 
 # PostgreSQL database connection details
 DB_HOST = os.environ.get('POSTGRES_HOST')
 DB_PORT = "5432"
 DB_NAME = os.environ.get('POSTGRES_DB')
 DB_USER = os.environ.get('POSTGRES_USER')
-DB_PASSWORD = os.environ.get('POSTGRES_PASSWORD') 
+DB_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+
+# Define Avro schema
+avro_schema = {
+    "type": "record",
+    "name": "WebEvent",
+    "fields": [
+        {"name":"event_id", "type": "string"},
+        {"name":"event_timestamp", "type": "string"},
+        {"name":"event_type", "type": "string"},
+        {"name":"page_url", "type": "string"},
+        {"name":"page_url_path", "type": "string"},
+        {"name":"referer_url", "type": "string"},
+        {"name":"referer_url_scheme", "type": "string"},
+        {"name":"referer_url_port", "type": "string"},
+        {"name":"referer_medium", "type": "string"},
+        {"name":"utm_medium", "type": "string"},
+        {"name":"utm_source", "type": "string"},
+        {"name":"utm_content", "type": "string"},
+        {"name":"utm_campaign", "type": "string"},
+        {"name":"click_id", "type": ["null", "string"]},
+        {"name":"geo_latitude", "type": ["null", "string"]},
+        {"name":"geo_longitude", "type": ["null", "string"]},
+        {"name":"geo_country", "type": "string"},
+        {"name":"geo_timezone", "type": "string"},
+        {"name":"geo_region_name", "type": "string"},
+        {"name":"ip_address", "type": "string"},
+        {"name":"browser_name", "type": "string"},
+        {"name":"browser_user_agent", "type": "string"},
+        {"name":"browser_language", "type": "string"},
+        {"name":"os", "type": "string"},
+        {"name":"os_name", "type": "string"},
+        {"name":"os_timezone", "type": "string"},
+        {"name":"device_type", "type": "string"},
+        {"name":"device_is_mobile", "type": ["null", "string", "boolean"]},
+        {"name":"user_custom_id", "type": "string"},
+        {"name":"user_domain_id", "type": ["null", "string"]}
+    ]
+}
+
+# Initialise the simulation
+simulation = Simulation(user_pool_size=100, sessions_per_day=1000)
+event = next(simulation.run(duration_seconds=60))
+
+# Write to Avro file
+with open("web_events.avro", "wb") as out_file:
+    fastavro.writer(out_file, avro_schema, [event])
 
 # Connect to the PostgreSQL database
 connection = psycopg2.connect(
@@ -42,103 +71,56 @@ cursor = connection.cursor()
 
 print("Connection sucessful!")
 
-# Create events table
-query = """
-CREATE TABLE events (
-    event_id TEXT,
-    event_timestamp TIMESTAMP,
-    event_type TEXT,
-    page_url TEXT,
-    page_url_path TEXT,
-    referer_url TEXT,
-    referer_url_scheme TEXT,
-    referer_url_port TEXT,
-    referer_medium TEXT,
-    utm_medium TEXT,
-    utm_source TEXT,
-    utm_content TEXT,
-    utm_campaign TEXT,
-    click_id UUID,
-    geo_latitude FLOAT,
-    geo_longitude FLOAT,
-    geo_country TEXT,
-    geo_timezone TEXT,
-    geo_region_name TEXT,
-    ip_address INET,
-    browser_name TEXT,
-    browser_user_agent TEXT,
-    browser_language TEXT,
-    os TEXT,
-    os_name TEXT,
-    os_timezone TEXT,
-    device_type TEXT,
-    device_is_mobile BOOLEAN,
-    user_custom_id TEXT,
-    user_domain_id UUID
-);
-"""
-cursor.execute(query)
+# Helper function to map Avro types to Postgres types
+def map_avro_to_postgres(avro_type):
+    type_mapping = {
+        "string": "TEXT",
+        "float": "REAL",
+        "timestamp-millis": "TIMESTAMP"
+    }
+    if isinstance(avro_type, list):
+        # Handle union types (e.g. ["nulll", "string"])
+        avro_type = [t for t in avro_type if t != "null"][0]
+    return type_mapping.get(avro_type, "TEXT")
 
-event_count = 0
+# Create table based on Avro schema
+table_name = "web_events"
+fields = avro_schema["fields"]
+columns = []
 
-column_names = [
-    "event_id",
-    "event_timestamp",
-    "event_type",
-    "page_url",
-    "page_url_path",
-    "referer_url",
-    "referer_url_scheme",
-    "referer_url_port",
-    "referer_medium",
-    "utm_medium",
-    "utm_source",
-    "utm_content",
-    "utm_campaign",
-    "click_id",
-    "geo_latitude",
-    "geo_longitude",
-    "geo_country",
-    "geo_timezone",
-    "geo_region_name",
-    "ip_address",
-    "browser_name",
-    "browser_user_agent",
-    "browser_language",
-    "os",
-    "os_name",
-    "os_timezone",
-    "device_type",
-    "device_is_mobile",
-    "user_custom_id",
-    "user_domain_id"
-    ]
+for field in fields:
+    column_name = field["name"]
+    avro_type = field["type"]
+    postgres_type = map_avro_to_postgres(avro_type)
+    columns.append(f"{column_name} {postgres_type}")
 
-columns_str = ", ".join(column_names)
+create_table_query = f"CREATE TABLE IF NOT EXISTS {table_name} ({', '.join(columns)})"
 
-# Generate events continuously and insert them into the database 
-for event in simulation.run(duration_seconds=60 * 60 * 24 * TOTAL_DAYS):  # Run for 31 days
-    # Increment the simulation time slightly (1 second between events)
-    simulation.cur_time += timedelta(seconds=1)
+cursor.execute(create_table_query)
+connection.commit()
 
-    # Extract values based on the dynamically determined columns
-    values = tuple(event[col] for col in column_names)
+print(f"Table '{table_name}' created successfully!")
 
-    # Insert the event into the PostgreSQL table
-    try:
-        insert_query = f"INSERT INTO events ({columns_str}) VALUES ({values})"
+# Insert data into PostgreSQL
+with open("web_events.avro", "rb") as avro_file:
+    reader = fastavro.reader(avro_file)
+    for record in reader:
+        columns = list(record.keys())
+        values = list(record.values())
+
+        # Replace None values with NULL and convert to appropriate types
+        values = [value if value is not None else None for value in values]
+
+        insert_query = sql.SQL("INSERT into {table} ({fields}) VALUES ({placeholsers})").format(
+            table=sql.Identifier(table_name),
+            fields=sql.SQL(', ').join(map(sql.Identifier, columns)),
+            placeholders=sql.SQL(', ').join(sql.Placeholder() * len(values))
+        )
         cursor.execute(insert_query, values)
-    except Exception as e:
-        print(f'Insertion failed: {e}')
-    event_count += 1
-    
-    # Break once the total number of events is reached
-    if event_count >= TOTAL_EVENTS:
-        break
 
 # Commit the transaction and close the connection
 connection.commit()
 cursor.close()
 connection.close()
 
-print(f"Inserted {event_count} events into the PostgreSQL 'events' table")
+print("Data inserted into the PostgreSQL 'web_events' table")
